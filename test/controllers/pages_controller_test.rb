437f8b73e3c833e1716db8e1fa8d07f581f1bd09
Kyle Horton <kyle@cplventures.com>
2022-05-16 23:00:41 -0400

    test: :white_check_mark: add root page tests

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,4 +5,21 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
+  test "root should have search bar" do
+    get root_url
+    assert_select "input"
+  end
+  test "root should have table" do
+    get root_url
+    assert_select "table"
+  end
+  test "root table should have correct headers" do
+    get root_url
+    assert_select "table" do
+      assert_select "th", text: "Ingredient"
+      assert_select "th", text: "Amount"
+      assert_select "th", text: "Expires"
+      assert_select "th", text: "Location"
+    end
+  end
 end
